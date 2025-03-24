e736d2783e6fd1652a00281d09c5eb652619da72
fatoma <fatomandaou5@gmail.com>
2025-03-24 10:47:21 +0000

    initial commit

---
+++ src/main/java/com/dch/Cine_Babemba/CineBabembaApplication.java
@@ -0,0 +1,13 @@
+package com.dch.Cine_Babemba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBabembaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBabembaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dch/Cine_Babemba/CineBabembaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dch.Cine_Babemba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBabembaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
